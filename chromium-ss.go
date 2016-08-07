e1f19a42b164b9e4ee31400bf6dab1b7f073f641
aarzilli <alessandro.arzilli@gmail.com>
2016-08-07 08:43:19 +0200

    Autodetect running browser.

--- chromium-ss.go
+++ chromium-ss.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
-	"fmt"
 	"strings"
 )
 
@@ -14,29 +14,48 @@ func must(err error) {
 	}
 }
 
-func main() {
-	p := os.Getenv("HOME")
-	var browser string
+func try(browser string) error {
 	var argv0 string
 
-	switch filepath.Base(os.Args[0]) {
-	case "google-chrome-ss":
-		browser = "google-chrome"
+	switch browser {
+	case "google-chrome":
 		argv0 = "google-chrome"
 	default:
-		browser = "chromium"
 		argv0 = "chromium-browser"
 	}
 
-	for _, c := range []string{ ".config", browser, "SingletonSocket" } {
-		p = filepath.Join(p, c)
+	p := filepath.Join(filepath.Join(filepath.Join(os.Getenv("HOME"), ".config"), browser), "SingletonSocket")
+
+	if _, err := os.Stat(p); err != nil {
+		return err
 	}
 
 	wd, _ := os.Getwd()
 
 	conn, err := net.Dial("unix", p)
-	must(err)
+	if err != nil {
+		return err
+	}
 	defer conn.Close()
 
-	fmt.Fprintf(conn, "START\000%s\000%s\000%s", wd, argv0, strings.Join(os.Args[1:], "\000"))
+	_, err = fmt.Fprintf(conn, "START\000%s\000%s\000%s", wd, argv0, strings.Join(os.Args[1:], "\000"))
+
+	return err
+
+}
+
+func main() {
+	switch filepath.Base(os.Args[0]) {
+	case "google-chrome-ss":
+		err := try("google-chrome")
+		if err != nil {
+			try("chromium")
+		}
+	default:
+		err := try("chromium")
+		if err != nil {
+			try("google-chrome")
+		}
+	}
+
 }
